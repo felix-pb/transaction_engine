3d5a406b8b69a465865d4d476cd430a3b6528f9e
felix-pb <f.poulinbelanger@gmail.com>
2021-10-26 12:24:09 -0400

    Tokio benchmark

--- examples/tokio-message-passing.rs
+++ examples/tokio-message-passing.rs
@@ -1,33 +1,38 @@
-use tokio::io::AsyncReadExt;
-use tokio::net::TcpListener;
-use transaction_engine::TransactionEngine;
+use std::time::Instant;
+use transaction_engine::{ClientId, TransactionEngine};
+
+const NUMBER_OF_TRANSACTIONS: u32 = 65_536_000;
 
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
-    let (tx, rx) = std::sync::mpsc::channel();
-    std::thread::spawn(move || {
-        let mut engine = TransactionEngine::init();
-        // blocking here is okay because this thread is not running in the tokio
-        // worker thread pool. in real scenario, this thread should be pinned to a core.
-        while let Ok(_transaction) = rx.recv() {
-            // depending on the transaction, call the approprate engine method
-            engine.deposit(0, 0, 0).unwrap();
+    let mut engine = TransactionEngine::init();
+    let (sender, mut receiver) = tokio::sync::mpsc::channel(NUMBER_OF_TRANSACTIONS as usize);
+
+    let now = Instant::now();
+
+    let task1 = tokio::spawn(async move {
+        while let Some((deposit, client, tx, amount)) = receiver.recv().await {
+            if deposit {
+                engine.deposit(client, tx, amount).unwrap();
+            } else {
+                engine.withdrawal(client, tx, amount).unwrap();
+            }
         }
     });
-    loop {
-        let (mut stream, _) = listener.accept().await.unwrap();
-        let tx = tx.clone();
-        tokio::spawn(async move {
-            let mut buffer = [0; 1024];
-            loop {
-                let _n = stream.read(&mut buffer).await.unwrap();
-                // try to parse a transaction from the input data.
-                // then send the transaction structure to the engine thread.
-                // note that `send` is not a blocking method even if this
-                // is the std's mpsc channel and not the tokio version.
-                tx.send("transaction structure").unwrap();
+
+    let task2 = tokio::spawn(async move {
+        for tx in 0..NUMBER_OF_TRANSACTIONS {
+            let client = (tx % 65_536) as ClientId;
+            if tx < NUMBER_OF_TRANSACTIONS / 2 {
+                sender.send((true, client, tx, 1)).await.unwrap();
+            } else {
+                sender.send((false, client, tx, 1)).await.unwrap();
             }
-        });
-    }
+        }
+    });
+
+    task1.await.unwrap();
+    task2.await.unwrap();
+
+    dbg!(now.elapsed());
 }
--- examples/tokio-shared-state.rs
+++ examples/tokio-shared-state.rs
@@ -1,24 +1,28 @@
-use std::sync::{Arc, Mutex};
-use tokio::io::AsyncReadExt;
-use tokio::net::TcpListener;
-use transaction_engine::TransactionEngine;
+use parking_lot::Mutex;
+use std::sync::Arc;
+use std::time::Instant;
+use transaction_engine::{ClientId, TransactionEngine};
+
+const NUMBER_OF_TRANSACTIONS: u32 = 65_536_000;
 
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
     let engine = Arc::new(Mutex::new(TransactionEngine::init()));
-    loop {
-        let (mut stream, _) = listener.accept().await.unwrap();
-        let engine = engine.clone();
-        tokio::spawn(async move {
-            let mut buffer = [0; 1024];
-            loop {
-                let _n = stream.read(&mut buffer).await.unwrap();
-                // try to parse a transaction from the input data.
-                // then call appropriate engine method by locking the mutex
-                // but hold the guard for as short a time as possible.
-                engine.lock().unwrap().deposit(0, 0, 0).unwrap();
+
+    let now = Instant::now();
+
+    let task = tokio::spawn(async move {
+        for tx in 0..NUMBER_OF_TRANSACTIONS {
+            let client = (tx % 65_536) as ClientId;
+            if tx < NUMBER_OF_TRANSACTIONS / 2 {
+                engine.lock().deposit(client, tx, 1).unwrap();
+            } else {
+                engine.lock().withdrawal(client, tx, 1).unwrap();
             }
-        });
-    }
+        }
+    });
+
+    task.await.unwrap();
+
+    dbg!(now.elapsed());
 }
